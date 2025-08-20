81a12d52a719885edfc5b6f7087952db504a87f8
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-08-20 15:49:48 +0300

    Optimized code

--- client.py
+++ client.py
@@ -22,6 +22,45 @@ id_chat = 3
 
 prev_time = time.time()
 
+packet_ids = {
+    1: 15,
+    4: 8,
+    5: 10,
+    6: 12,
+    7: 9,
+    8: 2,
+    9: 1,
+    0x0d: 41,
+    0x0e: 11,
+    0x10: 2,
+    0x11: 14,
+    0x12: 5,
+    0x13: 5,
+    0x15: 24,
+    0x16: 8,
+    0x1b: 18,
+    0x1c: 10,
+    0x1d: 4,
+    0x1e: 4,
+    0x1f: 7,
+    0x20: 6,
+    0x21: 9,
+    0x22: 18,
+    0x26: 5,
+    0x27: 8,
+    0x32: 9,
+    0x35: 11,
+    0x36: 12,
+    0x3d: 17,
+    0x46: 1,
+    0x47: 17,
+    0x65: 1,
+    0x69: 5,
+    0x6a: 4,
+    0xc8: 5
+}
+
+
 # Chatgpt cooked this color map
 mc_color_map = {
     "§0": "\x1b[30m",
@@ -53,7 +92,7 @@ def translate_colors(msg: str) -> str:
         msg = msg.replace(code, ansi)
     return msg + "\033[0m"
 
-print(translate_colors("§6Golden §e2 §f- §aWelcome to the server!"))
+#print(translate_colors("§6Golden §e2 §f- §aWelcome to the server!"))
 
 def send_packet(s, id, data):
     s.sendall(struct.pack(f'>b', id) + data)
@@ -83,6 +122,125 @@ input_thread.daemon = True
 
 st = False
 
+def handle_packet(s, packet_id):
+    # --- Special handlers first ---
+    if packet_id == id_handshake:
+        length = struct.unpack('>h', s.recv(2))[0]
+        temp2 = s.recv(length * 2).decode('utf-16-be')
+        print(temp2)
+        if temp2 == "-":
+            send_packet(
+                s, id_login,
+                struct.pack('>i', 14) +
+                encode_string16(username) +
+                struct.pack('>qb', 0, 0)
+            )
+        return
+
+    if packet_id == id_chat:
+        length = struct.unpack('>h', s.recv(2))[0]
+        msg = s.recv(length * 2).decode('utf-16-be')
+        print_formatted_text(ANSI(translate_colors(msg)))
+        return
+
+    if packet_id == 0xff:  # kick
+        length = struct.unpack('>h', s.recv(2))[0]
+        reason = s.recv(length * 2).decode('utf-16-be')
+        print("kick:", reason)
+        return "disconnect"
+
+    # --- Fixed length packets from packet_ids dict ---
+    if packet_id in packet_ids:
+        s.recv(packet_ids[packet_id])
+        return
+
+    # --- Special variable-length ones (examples) ---
+    if packet_id == 0x0f:
+        s.recv(10)
+        if struct.unpack('>h', s.recv(2))[0] >= 0:
+            s.recv(3)
+        return
+
+    if packet_id == 0x14:
+        s.recv(4)
+        length = struct.unpack('>h', s.recv(2))[0]
+        s.recv(length + 16)
+        return
+
+    if packet_id == 0x17:
+        s.recv(17)
+        if struct.unpack('>i', s.recv(4))[0] > 0:
+            s.recv(6)
+        return
+
+    if packet_id == 0x18:
+        s.recv(19)
+        val = s.recv(1)
+        while val != b'\x7f':
+            val = s.recv(1)
+        return
+
+    if packet_id == 0x28:
+        s.recv(4)
+        val = s.recv(1)
+        while val != b'\x7f':
+            val = s.recv(1)
+        return
+
+    if packet_id == 0x33:
+        s.recv(13)
+        s.recv(struct.unpack('>i', s.recv(4))[0])
+        return
+
+    if packet_id == 0x34:
+        s.recv(8)
+        length = struct.unpack('>h', s.recv(2))[0]
+        s.recv(4 * length)
+        return
+
+    if packet_id == 0x3c:
+        s.recv(28)
+        s.recv(struct.unpack('>i', s.recv(4))[0] * 3)
+        return
+
+    if packet_id == 0x64:
+        s.recv(2)
+        length = struct.unpack('>h', s.recv(2))[0]
+        s.recv(length)
+        s.recv(1)
+        return
+
+    if packet_id == 0x67:
+        s.recv(3)
+        if struct.unpack('>h', s.recv(2))[0] != -1:
+            s.recv(3)
+        return
+
+    if packet_id == 0x68:
+        s.recv(1)
+        count = struct.unpack('>h', s.recv(2))[0]
+        for _ in range(count):
+            item_id = struct.unpack('>h', s.recv(2))[0]
+            if item_id != -1:
+                s.recv(3)
+        return
+
+    if packet_id == 0x82:
+        s.recv(10)
+        for _ in range(4):  # four strings
+            length = struct.unpack('>h', s.recv(2))[0]
+            s.recv(length)
+        return
+
+    if packet_id == 0x83:
+        s.recv(4)
+        s.recv(struct.unpack('>B', s.recv(1))[0])
+        return
+
+    # --- Fallback ---
+    # print(f"Unknown packet id: {hex(packet_id)}")
+
+
 def m():
     global prev_time
     global has_logged_in
@@ -110,180 +268,15 @@ def m():
                     return
 
         while True:
-            if time.time() - prev_time > 0.5:
-                if not has_logged_in:
-                    #send_packet(s, id_chat, encode_string16(login_command))
-                    has_logged_in = True
-                send_packet(s, id_keepalive, b'')
-                prev_time = time.time()
-
             try:
                 data = s.recv(1)
-                if data == b'':
-                    #m()
-                    #return
+                if not data:
                     continue
-                
                 packet_id = struct.unpack('>B', data)[0]
-
-
-                if packet_id == 0:
-                    pass
-                if packet_id == 1:
-                    s.recv(15)
-                if packet_id == id_handshake:
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    temp2 = s.recv(length * 2).decode('utf-16-be')
-                    print(temp2)
-                    if temp2 == "-":
-                        send_packet(s, id_login, struct.pack('>i', 14) + encode_string16(username) + struct.pack('>qb', 0, 0))
-                if packet_id == id_chat:
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    print_formatted_text(ANSI(translate_colors(s.recv(length * 2).decode('utf-16-be'))))
-                if packet_id == 4:
-                    s.recv(8)
-                if packet_id == 5:
-                    s.recv(10)
-                if packet_id == 6:
-                    s.recv(12)
-                if packet_id == 7:
-                    s.recv(9)
-                if packet_id == 8:
-                    s.recv(2)
-                if packet_id == 9:
-                    s.recv(1)
-                if packet_id == 0x0d:
-                    s.recv(41)
-                if packet_id == 0x0e:
-                    s.recv(11)
-                if packet_id == 0x0f:
-                    s.recv(10)
-                    if struct.unpack('>h', s.recv(2))[0] >= 0:
-                        s.recv(3)
-                if packet_id == 0x10:
-                    s.recv(2)
-                if packet_id == 0x11:
-                    s.recv(14)
-                if packet_id == 0x12:
-                    s.recv(5)
-                if packet_id == 0x13:
-                    s.recv(5)
-                if packet_id == 0x14:
-                    s.recv(4)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length + 16)
-                if packet_id == 0x15:
-                    s.recv(24)
-                if packet_id == 0x16:
-                    s.recv(8)
-                if packet_id == 0x17:
-                    s.recv(17)
-                    if struct.unpack('>i', s.recv(4))[0] > 0:
-                        s.recv(6)
-                if packet_id == 0x18:
-                    s.recv(19)
-                    val = s.recv(1)
-                    while val != b'\x7f':
-                        val = s.recv(1)
-                if packet_id == 0x19:
-                    s.recv(4)
-                    s.recv(struct.unpack('>h', s.recv(2))[0] + 16)
-                if packet_id == 0x1b:
-                    s.recv(18)
-                if packet_id == 0x1c:
-                    s.recv(10)
-                if packet_id == 0x1d:
-                    s.recv(4)
-                if packet_id == 0x1e:
-                    s.recv(4)
-                if packet_id == 0x1f:
-                    s.recv(7)
-                if packet_id == 0x20:
-                    s.recv(6)
-                if packet_id == 0x21:
-                    s.recv(9)
-                if packet_id == 0x22:
-                    s.recv(18)
-                if packet_id == 0x26:
-                    s.recv(5)
-                if packet_id == 0x27:
-                    s.recv(8)
-                if packet_id == 0x28:
-                    s.recv(4)
-                    val = s.recv(1)
-                    while val != b'\x7f':
-                        val = s.recv(1)
-                if packet_id == 0x32:
-                    s.recv(9)
-                if packet_id == 0x33:
-                    s.recv(13)
-                    s.recv(struct.unpack('>i', s.recv(4))[0])
-                if packet_id == 0x34:
-                    s.recv(8)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(4 * length)
-                if packet_id == 0x35:
-                    s.recv(11)
-                if packet_id == 0x36:
-                    s.recv(12)
-                if packet_id == 0x3c:
-                    s.recv(28)
-                    s.recv(struct.unpack('>i', s.recv(4))[0] * 3)
-                if packet_id == 0x3d:
-                    s.recv(17)
-                if packet_id == 0x46:
-                    s.recv(1)
-                if packet_id == 0x47:
-                    s.recv(17)
-                if packet_id == 0x64:
-                    s.recv(2)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length)
-                    s.recv(1)
-                if packet_id == 0x65:
-                    s.recv(1)
-                if packet_id == 0x67:
-                    s.recv(3)
-                    if struct.unpack('>h', s.recv(2))[0] != -1:
-                        s.recv(3)
-                if packet_id == 0x68:
-                    s.recv(1)
-                    count = struct.unpack('>h', s.recv(2))[0]
- 
-                    for slot in range(count):
-                        item_id = struct.unpack('>h', s.recv(2))[0]
-                        if item_id != -1:
-                            s.recv(3)
-
-                if packet_id == 0x69:
-                    s.recv(5)
-                if packet_id == 0x6a:
-                    s.recv(4)
-                if packet_id == 0x82:
-                    s.recv(10)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length)
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    s.recv(length)
-                if packet_id == 0x83:
-                    s.recv(4)
-                    s.recv(struct.unpack('>B', s.recv(1))[0])
-                if packet_id == 0xc8:
-                    s.recv(5)
-                if packet_id == 0xff:
-                    print("kick")
-                    length = struct.unpack('>h', s.recv(2))[0]
-                    print(s.recv(length * 2).decode('utf-16-be'))
-
-                    #m()
+                result = handle_packet(s, packet_id)
+                if result == "disconnect":
                     return
-
             except socket.timeout:
-                #m()
                 return
 
 m()
